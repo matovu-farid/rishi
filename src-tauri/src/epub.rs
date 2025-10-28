6a52ad79bdc9cfc0263d58f0a0b0c9b1328302f4
matovu-farid <matovu90@gmail.com>
2025-10-28 20:39:24 +0300

    Update dependencies and refactor command handling in Tauri application
    - Add new dependencies: `percent-encoding`, `thiserror`, and `xml-rs` to Cargo.toml and Cargo.lock.
    - Refactor command handling in lib.rs to utilize a separate `commads` module for better organization and clarity.
    - Remove unused code related to file handling and unzipping, streamlining the codebase.

---
+++ src-tauri/src/epub.rs
@@ -0,0 +1,4 @@
+mod xmlutils;
+
+pub mod archive;
+pub mod docs;
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,60 +1,8 @@
-use std::fs;
-use std::fs::File;
-use std::io;
-use std::path::{Path, PathBuf};
-use zip::ZipArchive;
+// pub mod epub;
+
+mod commads;
+
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
-#[tauri::command]
-fn is_dev() -> bool {
-    tauri::is_dev()
-}
-
-#[tauri::command]
-fn unzip(file_path: &str, out_dir: &str) -> Result<PathBuf, String> {
-    println!(
-        "unzip called with file_path: {:?}, out_dir: {:?}",
-        file_path, out_dir
-    );
-    let file = File::open(file_path).map_err(|e| e.to_string())?;
-    let mut archive = ZipArchive::new(file).map_err(|e| e.to_string())?;
-
-    let output_dir = Path::new(out_dir);
-    fs::create_dir_all(&output_dir).map_err(|e| e.to_string())?;
-
-    // Extract all files (like AdmZip's `extractAllTo`)
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).map_err(|e| e.to_string())?;
-        let outpath = output_dir.join(file.name());
-
-        if file.name().ends_with('/') {
-            fs::create_dir_all(&outpath).map_err(|e| e.to_string())?;
-        } else {
-            if let Some(parent) = outpath.parent() {
-                fs::create_dir_all(parent).map_err(|e| e.to_string())?;
-            }
-            let mut outfile = File::create(&outpath).map_err(|e| e.to_string())?;
-            io::copy(&mut file, &mut outfile).map_err(|e| e.to_string())?;
-        }
-    }
-
-    // Copy the original zip file into the extracted folder (AdmZip analog)
-    let zip_filename = Path::new(file_path)
-        .file_name()
-        .unwrap_or_default()
-        .to_string_lossy()
-        .to_string();
-    let new_zip_file_path = output_dir.join(zip_filename);
-    fs::copy(file_path, &new_zip_file_path).map_err(|e| e.to_string())?;
-
-    // println!("File was copied to {:?}", new_zip_file_path);
-
-    Ok(output_dir.to_path_buf())
-}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
@@ -63,7 +11,11 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, is_dev, unzip])
+        .invoke_handler(tauri::generate_handler![
+            commads::greet,
+            commads::is_dev,
+            commads::unzip
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
