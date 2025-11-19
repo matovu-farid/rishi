c492bf5086c4c30dbd46646de1a1209d4e714cfc
matovu-farid <matovu90@gmail.com>
2025-11-19 11:51:48 +0300

    Configure depplinking

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -8,11 +8,13 @@ mod shared;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_deep_link::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_os::init())
         .invoke_handler(tauri::generate_handler![
             commands::is_dev,
             commands::unzip,
--- src-tauri/src/shared/mod.rs
+++ src-tauri/src/shared/mod.rs
@@ -1,4 +1,2 @@
-pub mod types;
 pub mod books;
-
-
+pub mod types;
